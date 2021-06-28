e57ecf8ebb93f0b770d83966f29ea4680b2b98c8
maxwelluchoa <maxwellmagus@gmail.com>
2021-06-28 19:01:02 -0400

    07 - DrawerLayout

---
+++ app/src/main/java/com/max/firebaseapp/NavigationActivity.java
@@ -0,0 +1,30 @@
+package com.max.firebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    private ImageView btnMenu;
+    private DrawerLayout drawerLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        btnMenu = findViewById(R.id.navigation_icon);
+        drawerLayout = findViewById(R.id.nav_drawerLayout);
+
+        btnMenu.setOnClickListener(view ->{
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
+    }
+}
